e972d93f66fe72b58bbe140312750b7067062ea5
annag-lam <alam@captechconsulting.com>
2026-01-13 17:33:48 +0000

    worked on lab-2-2 and school in-class exercises

---
+++ school-5-insert.sql
@@ -0,0 +1,15 @@
+-- INSERT INTO _____ (
+--     ...columns
+-- ) VALUES (
+--     ...values
+-- )
+
+INSERT INTO students (
+    first_name,
+    last_name,
+    email
+) VALUES (
+    "Anna",
+    "Lam",
+    "anna.gk.lam@gmail.com"
+)
